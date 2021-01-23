07490776468ee66a8c8bcde0716cfecd02252e89
Ngo <ngomalalibo@yahoo.com>
2021-01-23 10:32:41 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/tag_build.sh
+++ travis_scripts/tag_build.sh
@@ -4,10 +4,11 @@ echo "Copying files to AWS instance"
 scp -i all-stock-key-pair.pem docker-compose.yml ubuntu@ec2-18-184-55-40.eu-central-1.compute.amazonaws.com:.
 scp -i all-stock-key-pair.pem travis_scripts/launch_build.sh ubuntu@ec2-18-184-55-40.eu-central-1.compute.amazonaws.com:.
 
-echo "Starting Kafka and Zookeeper"
+echo "Copying Kafka script to AWS Instance"
 #Start Kafka
 yes | scp -i all-stock-key-pair.pem travis_scripts/start_kafka.sh ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com:.
-yes | ssh -i "all-stock-key-pair.pem" ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com 'bash start_kafka.sh'
+echo "Starting Kafka and Zookeeper"
+ssh -i "all-stock-key-pair.pem" ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com 'bash start_kafka.sh'
 echo "Started Kafka and Zookeeper"
 
 echo "Tagging build with $BUILD_NAME"
