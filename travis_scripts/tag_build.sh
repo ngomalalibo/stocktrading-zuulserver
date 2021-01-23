ddfe50bd7a3c5c40f085a4570b02c89483db82fa
Ngo <ngomalalibo@yahoo.com>
2021-01-23 10:17:28 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/tag_build.sh
+++ travis_scripts/tag_build.sh
@@ -4,9 +4,11 @@ echo "Copying files to AWS instance"
 scp -i all-stock-key-pair.pem docker-compose.yml ubuntu@ec2-18-184-55-40.eu-central-1.compute.amazonaws.com:.
 scp -i all-stock-key-pair.pem travis_scripts/launch_build.sh ubuntu@ec2-18-184-55-40.eu-central-1.compute.amazonaws.com:.
 
+echo "Starting Kafka and Zookeeper"
 #Start Kafka
-#yes | scp -i all-stock-key-pair.pem travis_scripts/start_kafka.sh ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com:.
-#yes | ssh -i "all-stock-key-pair.pem" ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com 'bash start_kafka.sh'
+yes | scp -i all-stock-key-pair.pem travis_scripts/start_kafka.sh ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com:.
+yes | ssh -i "all-stock-key-pair.pem" ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com 'bash start_kafka.sh'
+echo "Started Kafka and Zookeeper"
 
 echo "Tagging build with $BUILD_NAME"
 export TARGET_URL="https://api.github.com/repos/ngomalalibo/stocktradingmicroservices/releases?access_token=$GITHUB_TOKEN"
